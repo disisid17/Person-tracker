cd9faa09dd79d3843e252ed15e4f6b9869d54111
disisid17 <77601095+disisid17@users.noreply.github.com>
2023-12-06 19:16:14 -0500

    Update detect.py

--- gstreamer/detect.py
+++ gstreamer/detect.py
@@ -32,6 +32,7 @@ python3 detect.py \
   --model ${TEST_DATA}/mobilenet_ssd_v2_coco_quant_postprocess_edgetpu.tflite \
   --labels ${TEST_DATA}/coco_labels.txt
 """
+import gradio as gr
 import argparse
 import collections
 import common
@@ -60,14 +61,14 @@ def shadow_text(dwg, x, y, text, font_size=20):
 
 
 def generate_svg(src_size, inference_size, inference_box, objs, labels, text_lines, trdata, trackerFlag):
+    count=0
     dwg = svgwrite.Drawing('', size=src_size)
     src_w, src_h = src_size
     inf_w, inf_h = inference_size
     box_x, box_y, box_w, box_h = inference_box
     scale_x, scale_y = src_w / box_w, src_h / box_h
 
-    for y, line in enumerate(text_lines, start=1):
-        shadow_text(dwg, 10, y*20, line)
+    
     if trackerFlag and (np.array(trdata)).size:
         for td in trdata:
             x0, y0, x1, y1, trackID = td[0].item(), td[1].item(
@@ -80,22 +81,24 @@ def generate_svg(src_size, inference_size, inference_box, objs, labels, text_lin
                 if (area > overlap):
                     overlap = area
                     obj = ob
-
-            # Relative coordinates.
-            x, y, w, h = x0, y0, x1 - x0, y1 - y0
-            # Absolute coordinates, input tensor space.
-            x, y, w, h = int(x * inf_w), int(y *
-                                             inf_h), int(w * inf_w), int(h * inf_h)
-            # Subtract boxing offset.
-            x, y = x - box_x, y - box_y
-            # Scale to source coordinate space.
-            x, y, w, h = x * scale_x, y * scale_y, w * scale_x, h * scale_y
-            percent = int(100 * obj.score)
-            label = '{}% {} ID:{}'.format(
-                percent, labels.get(obj.id, obj.id), int(trackID))
-            shadow_text(dwg, x, y - 5, label)
-            dwg.add(dwg.rect(insert=(x, y), size=(w, h),
-                             fill='none', stroke='red', stroke_width='2'))
+                    
+            if labels.get(obj.id, obj.id) == 'person':
+                count +=1
+                # Relative coordinates.
+                x, y, w, h = x0, y0, x1 - x0, y1 - y0
+                # Absolute coordinates, input tensor space.
+                x, y, w, h = int(x * inf_w), int(y *
+                                                 inf_h), int(w * inf_w), int(h * inf_h)
+                # Subtract boxing offset.
+                x, y = x - box_x, y - box_y
+                # Scale to source coordinate space.
+                x, y, w, h = x * scale_x + w*scale_x/2, y * scale_y+ h*scale_y/2, w * scale_x/10, h * scale_y/10
+                percent = int(100 * obj.score)
+                label = '{}% {} ID:{}'.format(
+                    percent, labels.get(obj.id, obj.id), int(trackID))
+                shadow_text(dwg, x, y - 5, label)
+                dwg.add(dwg.rect(insert=(x, y), size=(10, 10),
+                                 fill='none', stroke='red', stroke_width='5'))
     else:
         for obj in objs:
             x0, y0, x1, y1 = list(obj.bbox)
@@ -107,13 +110,19 @@ def generate_svg(src_size, inference_size, inference_box, objs, labels, text_lin
             # Subtract boxing offset.
             x, y = x - box_x, y - box_y
             # Scale to source coordinate space.
-            x, y, w, h = x * scale_x, y * scale_y, w * scale_x, h * scale_y
+            x, y, w, h = x * scale_x + w*scale_x/2, y * scale_y+ h*scale_y/2, w * scale_x/10, h * scale_y/10
             percent = int(100 * obj.score)
-            label = '{}% {}'.format(percent, labels.get(obj.id, obj.id))
-            shadow_text(dwg, x, y - 5, label)
-            dwg.add(dwg.rect(insert=(x, y), size=(w, h),
-                             fill='none', stroke='red', stroke_width='2'))
-    return dwg.tostring()
+            if labels.get(obj.id, obj.id) == 'person':
+                count+=1
+                label = '{}% {}'.format(percent, labels.get(obj.id, obj.id))
+                shadow_text(dwg, x, y - 5, label)
+                dwg.add(dwg.rect(insert=(x, y), size=(10, 10), fill='none', stroke='red', stroke_width='5'))
+    text_lines[2] = '# of people: {}'.format(count)
+    for y, line in enumerate(text_lines, start=1):
+        shadow_text(dwg, 10, y*20, line)
+    #print(dwg.tostring())
+    #print("\n")
+    return dwg.tostring(),count
 
 
 class BBox(collections.namedtuple('BBox', ['xmin', 'ymin', 'xmax', 'ymax'])):
@@ -144,6 +153,7 @@ def get_output(interpreter, score_threshold, top_k, image_scale=1.0):
 
 def main():
     default_model_dir = '../models'
+    count = 0
     default_model = 'mobilenet_ssd_v2_coco_quant_postprocess_edgetpu.tflite'
     default_labels = 'coco_labels.txt'
     parser = argparse.ArgumentParser()
@@ -151,9 +161,9 @@ def main():
                         default=os.path.join(default_model_dir, default_model))
     parser.add_argument('--labels', help='label file path',
                         default=os.path.join(default_model_dir, default_labels))
-    parser.add_argument('--top_k', type=int, default=3,
+    parser.add_argument('--top_k', type=int, default=20,
                         help='number of categories with highest score to display')
-    parser.add_argument('--threshold', type=float, default=0.1,
+    parser.add_argument('--threshold', type=float, default=0.5,
                         help='classifier score threshold')
     parser.add_argument('--videosrc', help='Which video source to use. ',
                         default='/dev/video0')
@@ -175,7 +185,8 @@ def main():
     # Average fps over last 30 frames.
     fps_counter = common.avg_fps_counter(30)
 
-    def user_callback(input_tensor, src_size, inference_box, mot_tracker):
+    def user_callback(input_tensor,src_size, inference_box, mot_tracker):
+        #gr.Interface(fn = lambda:x+"",inputs = 'text',outputs = "text").launch()
         nonlocal fps_counter
         start_time = time.monotonic()
         common.set_input(interpreter, input_tensor)
@@ -202,16 +213,22 @@ def main():
                 trackerFlag = True
             text_lines = [
                 'Inference: {:.2f} ms'.format((end_time - start_time) * 1000),
-                'FPS: {} fps'.format(round(next(fps_counter))), ]
+                'FPS: {} fps'.format(round(next(fps_counter))),'', ]
+        
         if len(objs) != 0:
-            return generate_svg(src_size, inference_size, inference_box, objs, labels, text_lines, trdata, trackerFlag)
+            imga,count = generate_svg(src_size, inference_size, inference_box, objs, labels, text_lines, trdata, trackerFlag)
+            #print(count)
+            return imga,count
+        return "",0
+        
 
-    result = gstreamer.run_pipeline(user_callback,
+    gstreamer.run_pipeline(user_callback,
                                     src_size=(640, 480),
                                     appsink_size=inference_size,
                                     trackerName=args.tracker,
                                     videosrc=args.videosrc,
                                     videofmt=args.videofmt)
+    #return count
 
 
 if __name__ == '__main__':
